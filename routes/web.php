eb541948293745966d00c1f800e5fc900f1c68dd
Kantrawibawa10 <githubsaya@gmail.com>
2023-10-10 10:56:43 +0800

    inertia install

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\ProfileController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    Log::info('Users mengakses halaman welcome');
     return view('welcome');
 });
